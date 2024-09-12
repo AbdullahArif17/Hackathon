b747bbc5c1efba7b41a5d84df73f1938ee1e8805
Abdullah Arif <160360851+AbdullahArif17@users.noreply.github.com>
2024-09-12 23:20:09 +0500

    new features

--- main.js
+++ main.js
@@ -1,44 +1,97 @@
 var _a;
-(_a = document.getElementById('resume')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', function (event) {
+(_a = document.getElementById("resume")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", function (event) {
     event.preventDefault();
     console.log("Form submitted, preventing default");
-    // Get the input elements
-    var elementName = document.getElementById('name');
-    var elementEmail = document.getElementById('email');
-    var elementPhone = document.getElementById('phone');
-    var elementEducation = document.getElementById('education');
-    var elementExperience = document.getElementById('experience');
-    var elementSkill = document.getElementById('skill');
-    // Check if any of the elements are null
-    if (elementName && elementEmail && elementPhone && elementEducation && elementExperience && elementSkill) {
-        // Log retrieved values for debugging
-        console.log("All form elements found.");
-        console.log("Name:", elementName.value);
-        console.log("Email:", elementEmail.value);
-        console.log("Phone:", elementPhone.value);
-        console.log("Education:", elementEducation.value);
-        console.log("Experience:", elementExperience.value);
-        console.log("Skills:", elementSkill.value);
-        // Retrieve and trim the values from the input elements
-        var name_1 = elementName.value.trim();
-        var email = elementEmail.value.trim();
-        var phone = elementPhone.value.trim();
-        var education = elementEducation.value.trim();
-        var experience = elementExperience.value.trim();
-        var skill = elementSkill.value.trim();
-        // Create the resume output HTML
-        var resumeOut = "\n            <h2>Resume</h2>\n            <p><strong>Name:</strong> ".concat(name_1, "</p>\n            <p><strong>Email:</strong> ").concat(email, "</p>\n            <p><strong>Phone:</strong> ").concat(phone, "</p>\n            <h3>Education:</h3>\n            <p>").concat(education, "</p>\n            <h3>Experience:</h3>\n            <p>").concat(experience, "</p>\n            <h3>Skills:</h3>\n            <p>").concat(skill, "</p>\n        ");
-        // Display the resume output
-        var resumeOutElement = document.getElementById('resumeOut');
-        if (resumeOutElement) {
-            resumeOutElement.innerHTML = resumeOut;
-            console.log("Resume displayed successfully");
-        }
-        else {
-            console.error('The resumeOut element is missing');
-        }
+    var formValues = getFormValues();
+    if (formValues) {
+        var name_1 = formValues.name, email = formValues.email, phone = formValues.phone, add = formValues.add, education = formValues.education, experience = formValues.experience, skill = formValues.skill, profilePicURL = formValues.profilePicURL, username = formValues.username;
+        var uniquePath = "".concat(username.replace(/\s+/g, "_"), "_cv.html");
+        var resumeOut = generateResumeHTML(profilePicURL, name_1, email, phone, add, education, experience, skill);
+        displayResume(resumeOut);
     }
     else {
-        console.error('One or more form elements are missing');
+        console.error("One or more form elements are missing");
     }
 });
+function getFormValues() {
+    var _a;
+    var elementpic = document.getElementById("profilePic");
+    var elementName = document.getElementById("name");
+    var elementEmail = document.getElementById("email");
+    var elementPhone = document.getElementById("phone");
+    var elementadd = document.getElementById("add");
+    var elementEducation = document.getElementById("education");
+    var elementExperience = document.getElementById("experience");
+    var elementSkill = document.getElementById("skill");
+    var elementUser = document.getElementById("username");
+    if (elementpic &&
+        elementName &&
+        elementEmail &&
+        elementPhone &&
+        elementadd &&
+        elementEducation &&
+        elementExperience &&
+        elementSkill &&
+        elementUser) {
+        return {
+            name: elementName.value.trim(),
+            email: elementEmail.value.trim(),
+            phone: elementPhone.value.trim(),
+            add: elementadd.value.trim(),
+            education: elementEducation.value.trim(),
+            experience: elementExperience.value.trim(),
+            skill: elementSkill.value.trim(),
+            profilePicURL: ((_a = elementpic.files) === null || _a === void 0 ? void 0 : _a[0])
+                ? URL.createObjectURL(elementpic.files[0])
+                : "",
+            username: elementUser.value.trim(),
+        };
+    }
+    return null;
+}
+function generateResumeHTML(profilePicURL, name, email, phone, add, education, experience, skill) {
+    return "\n        <h2>Resume</h2>\n        ".concat(profilePicURL
+        ? "<img src=\"".concat(profilePicURL, "\" alt=\"Profile Picture\" class=\"profilePic\">")
+        : "", "\n        <p><strong>Name:</strong><span id=\"edit-name\" class=\"editable\"> ").concat(name, "</span></p>\n        <p><strong>Email:</strong><span id=\"edit-email\" class=\"editable\">").concat(email, "</span></p>\n        <p><strong>Phone:</strong><span id=\"edit-phone\" class=\"editable\"> ").concat(phone, "</span></p>\n        <p><strong>Address:</strong><span id=\"edit-add\" class=\"editable\"> ").concat(add, "</span></p> \n        <h3>Education:</h3>\n        <p id=\"edit-education\" class=\"editable\">").concat(education, "</p>\n        <h3>Experience:</h3>\n        <p id=\"edit-experience\" class=\"editable\">").concat(experience, "</p>\n        <h3>Skills:</h3>\n        <p id=\"edit-skills\" class=\"editable\">").concat(skill, "</p>\n    ");
+}
+function displayResume(resumeOut) {
+    var resumeOutElement = document.getElementById("resumeOut");
+    if (resumeOutElement) {
+        resumeOutElement.innerHTML = resumeOut;
+        var buttonContainer = document.createElement("div");
+        buttonContainer.id = "buttonContainer";
+        resumeOutElement.appendChild(buttonContainer);
+        var downloadButton = document.createElement("button");
+        downloadButton.textContent = "Download As PDF";
+        downloadButton.addEventListener("click", function () {
+            window.print();
+        });
+        buttonContainer.appendChild(downloadButton);
+        console.log("Resume displayed successfully");
+        makeEditable();
+    }
+}
+function makeEditable() {
+    var editableElements = document.querySelectorAll(".editable");
+    editableElements.forEach(function (element) {
+        element.addEventListener("click", function () {
+            var _a;
+            var currentElement = element;
+            var currentValue = currentElement.textContent || "";
+            if (currentElement.tagName === "P" || currentElement.tagName === "SPAN") {
+                var input_1 = document.createElement("input");
+                input_1.type = "text";
+                input_1.value = currentValue;
+                input_1.classList.add("editing-input");
+                currentElement.style.display = "none";
+                (_a = currentElement.parentNode) === null || _a === void 0 ? void 0 : _a.insertBefore(input_1, currentElement);
+                input_1.focus();
+                input_1.addEventListener("blur", function () {
+                    currentElement.textContent = input_1.value;
+                    currentElement.style.display = "";
+                    input_1.remove();
+                });
+            }
+        });
+    });
+}
--- main.ts
+++ main.ts
@@ -1,58 +1,170 @@
-document.getElementById('resume')?.addEventListener('submit', function(event) {
-    event.preventDefault();
+document.getElementById("resume")?.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    console.log("Form submitted, preventing default");
+  console.log("Form submitted, preventing default");
 
-    // Get the input elements
-    const elementName = document.getElementById('name') as HTMLInputElement | null;
-    const elementEmail = document.getElementById('email') as HTMLInputElement | null;
-    const elementPhone = document.getElementById('phone') as HTMLInputElement | null;
-    const elementEducation = document.getElementById('education') as HTMLInputElement | null;
-    const elementExperience = document.getElementById('experience') as HTMLInputElement | null;
-    const elementSkill = document.getElementById('skill') as HTMLInputElement | null;
+  const formValues = getFormValues();
 
-    // Check if any of the elements are null
-    if (elementName && elementEmail && elementPhone && elementEducation && elementExperience && elementSkill) {
-        // Log retrieved values for debugging
-        console.log("All form elements found.");
-        console.log("Name:", elementName.value);
-        console.log("Email:", elementEmail.value);
-        console.log("Phone:", elementPhone.value);
-        console.log("Education:", elementEducation.value);
-        console.log("Experience:", elementExperience.value);
-        console.log("Skills:", elementSkill.value);
+  if (formValues) {
+    const {
+      name,
+      email,
+      phone,
+      add,
+      education,
+      experience,
+      skill,
+      profilePicURL,
+      username,
+    } = formValues;
+    const uniquePath = `${username.replace(/\s+/g, "_")}_cv.html`;
 
-        // Retrieve and trim the values from the input elements
-        const name = elementName.value.trim();
-        const email = elementEmail.value.trim();
-        const phone = elementPhone.value.trim();
-        const education = elementEducation.value.trim();
-        const experience = elementExperience.value.trim();
-        const skill = elementSkill.value.trim();
+    const resumeOut = generateResumeHTML(
+      profilePicURL,
+      name,
+      email,
+      phone,
+      add,
+      education,
+      experience,
+      skill
+    );
 
-        // Create the resume output HTML
-        const resumeOut = `
-            <h2>Resume</h2>
-            <p><strong>Name:</strong> ${name}</p>
-            <p><strong>Email:</strong> ${email}</p>
-            <p><strong>Phone:</strong> ${phone}</p>
-            <h3>Education:</h3>
-            <p>${education}</p>
-            <h3>Experience:</h3>
-            <p>${experience}</p>
-            <h3>Skills:</h3>
-            <p>${skill}</p>
-        `;
-
-        // Display the resume output
-        const resumeOutElement = document.getElementById('resumeOut');
-        if (resumeOutElement) {
-            resumeOutElement.innerHTML = resumeOut;
-            console.log("Resume displayed successfully");
-        } else {
-            console.error('The resumeOut element is missing');
-        }
-    } else {
-        console.error('One or more form elements are missing');
-    }
+    displayResume(resumeOut);
+  } else {
+    console.error("One or more form elements are missing");
+  }
 });
+
+function getFormValues() {
+  const elementpic = document.getElementById(
+    "profilePic"
+  ) as HTMLInputElement | null;
+  const elementName = document.getElementById(
+    "name"
+  ) as HTMLInputElement | null;
+  const elementEmail = document.getElementById(
+    "email"
+  ) as HTMLInputElement | null;
+  const elementPhone = document.getElementById(
+    "phone"
+  ) as HTMLInputElement | null;
+  const elementadd = document.getElementById("add") as HTMLInputElement | null;
+  const elementEducation = document.getElementById(
+    "education"
+  ) as HTMLInputElement | null;
+  const elementExperience = document.getElementById(
+    "experience"
+  ) as HTMLInputElement | null;
+  const elementSkill = document.getElementById(
+    "skill"
+  ) as HTMLInputElement | null;
+  const elementUser = document.getElementById(
+    "username"
+  ) as HTMLInputElement | null;
+
+  if (
+    elementpic &&
+    elementName &&
+    elementEmail &&
+    elementPhone &&
+    elementadd &&
+    elementEducation &&
+    elementExperience &&
+    elementSkill &&
+    elementUser
+  ) {
+    return {
+      name: elementName.value.trim(),
+      email: elementEmail.value.trim(),
+      phone: elementPhone.value.trim(),
+      add: elementadd.value.trim(),
+      education: elementEducation.value.trim(),
+      experience: elementExperience.value.trim(),
+      skill: elementSkill.value.trim(),
+      profilePicURL: elementpic.files?.[0]
+        ? URL.createObjectURL(elementpic.files[0])
+        : "",
+      username: elementUser.value.trim(),
+    };
+  }
+  return null;
+}
+
+function generateResumeHTML(
+  profilePicURL,
+  name,
+  email,
+  phone,
+  add,
+  education,
+  experience,
+  skill
+) {
+  return `
+        <h2>Resume</h2>
+        ${
+          profilePicURL
+            ? `<img src="${profilePicURL}" alt="Profile Picture" class="profilePic">`
+            : ""
+        }
+        <p><strong>Name:</strong><span id="edit-name" class="editable"> ${name}</span></p>
+        <p><strong>Email:</strong><span id="edit-email" class="editable">${email}</span></p>
+        <p><strong>Phone:</strong><span id="edit-phone" class="editable"> ${phone}</span></p>
+        <p><strong>Address:</strong><span id="edit-add" class="editable"> ${add}</span></p> 
+        <h3>Education:</h3>
+        <p id="edit-education" class="editable">${education}</p>
+        <h3>Experience:</h3>
+        <p id="edit-experience" class="editable">${experience}</p>
+        <h3>Skills:</h3>
+        <p id="edit-skills" class="editable">${skill}</p>
+    `;
+}
+
+function displayResume(resumeOut) {
+  const resumeOutElement = document.getElementById("resumeOut");
+  if (resumeOutElement) {
+    resumeOutElement.innerHTML = resumeOut;
+
+    const buttonContainer = document.createElement("div");
+    buttonContainer.id = "buttonContainer";
+    resumeOutElement.appendChild(buttonContainer);
+
+    const downloadButton = document.createElement("button");
+    downloadButton.textContent = "Download As PDF";
+    downloadButton.addEventListener("click", () => {
+      window.print();
+    });
+    buttonContainer.appendChild(downloadButton);
+
+    console.log("Resume displayed successfully");
+    makeEditable();
+  }
+}
+
+function makeEditable() {
+  const editableElements = document.querySelectorAll(".editable");
+  editableElements.forEach((element) => {
+    element.addEventListener("click", function () {
+      const currentElement = element as HTMLElement;
+      const currentValue = currentElement.textContent || "";
+
+      if (currentElement.tagName === "P" || currentElement.tagName === "SPAN") {
+        const input = document.createElement("input");
+        input.type = "text";
+        input.value = currentValue;
+        input.classList.add("editing-input");
+
+        currentElement.style.display = "none";
+        currentElement.parentNode?.insertBefore(input, currentElement);
+        input.focus();
+
+        input.addEventListener("blur", () => {
+          currentElement.textContent = input.value;
+          currentElement.style.display = "";
+          input.remove();
+        });
+      }
+    });
+  });
+}
